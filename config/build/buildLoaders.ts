7381de89864fa1d40953d7560a34e40f1c62da9a
dreamybo1 <idelchiksasha@gmail.com>
2024-06-12 09:46:11 +0500

    babel + i18n extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -21,6 +21,28 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
       ],
     }
 
+    const babelLoader = {
+      test: /\.(js|jsx|tsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['@babel/preset-env', { targets: "defaults" }]
+          ],
+          "plugins": [
+            [
+              "i18next-extract", 
+              {
+                locales: ["ru", "en"],
+                keyAsDefaultValue: true
+              }
+            ],
+          ]
+        }
+      }
+    }
+
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
@@ -53,6 +75,7 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     return [
         svgLoader,
         fileLoader,
+        babelLoader,
         typescriptLoader, 
         cssLoader 
     ]
